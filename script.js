c8704f7fd09e785c7cf6e23b42a7f7553cbad893
tookey95 <petetookey@gmail.com>
2023-06-15 14:40:23 -0400

    added functionality for past/present/future

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
+dayjs.extend(window.dayjs_plugin_advancedFormat);
+
+var currentDay = dayjs().format("dddd, MMM Do");
+$("#currentDay").text(currentDay);
+
+var currentTime=dayjs().format("HH");
+console.log(currentTime);
+
+var userInput=$(".description");
+
+$(".time-block").each(function(){
+  var id = $(this).attr("id");
+
+  if (id < currentTime){
+    console.log(id)
+    console.log(currentTime)
+    $(this).addClass("past")
+  }
+
+  else if (id === currentTime){
+    $(this).addClass("present")
+  }
+
+  else if (id > currentTime){
+    $(this).addClass("future")
+  }
+})
+
 var date = $("#currentDay");
 date.text(dayjs().format("dddd, MMMM D"));
 var nth = date.text().slice(-1);
