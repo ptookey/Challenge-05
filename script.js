1bbccccad9d2784492ec0610309e85d8a9a00bcc
tookey95 <petetookey@gmail.com>
2023-06-15 14:43:47 -0400

    made the local storage keys more specific to avoid issues

--- script.js
+++ script.js
@@ -43,18 +43,18 @@ if (nth>3){
 
 
 $(".saveBtn").on("click", function () {
-  var key=$(this).parent().attr("id");
+  var key="workDaySchedulerHour-"+$(this).parent().attr("id");
   var userInput=$(this).siblings(".description").val();
 
   localStorage.setItem(key,userInput);    
 })
 
-$("#09 .description").val(localStorage.getItem("09"));
-$("#10 .description").val(localStorage.getItem("10"));
-$("#11 .description").val(localStorage.getItem("11"));
-$("#12 .description").val(localStorage.getItem("12"));
-$("#13 .description").val(localStorage.getItem("13"));
-$("#14 .description").val(localStorage.getItem("14"));
-$("#15 .description").val(localStorage.getItem("15"));
-$("#16 .description").val(localStorage.getItem("16"));
-$("#17 .description").val(localStorage.getItem("17"));
+$("#09 .description").val(localStorage.getItem("workDaySchedulerHour-09"));
+$("#10 .description").val(localStorage.getItem("workDaySchedulerHour-10"));
+$("#11 .description").val(localStorage.getItem("workDaySchedulerHour-11"));
+$("#12 .description").val(localStorage.getItem("workDaySchedulerHour-12"));
+$("#13 .description").val(localStorage.getItem("workDaySchedulerHour-13"));
+$("#14 .description").val(localStorage.getItem("workDaySchedulerHour-14"));
+$("#15 .description").val(localStorage.getItem("workDaySchedulerHour-15"));
+$("#16 .description").val(localStorage.getItem("workDaySchedulerHour-16"));
+$("#17 .description").val(localStorage.getItem("workDaySchedulerHour-17"));
