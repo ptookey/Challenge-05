682e33ce6d93ce09dd1687c8b4778398b63dd2ce
tookey95 <petetookey@gmail.com>
2023-06-15 14:41:12 -0400

    added local storage functionality

--- script.js
+++ script.js
@@ -40,3 +40,21 @@ if (nth>3){
 } else if (nth==0){
   date.text(date.text()+"th");
 }
+
+
+$(".saveBtn").on("click", function () {
+  var key=$(this).parent().attr("id");
+  var userInput=$(this).siblings(".description").val();
+
+  localStorage.setItem(key,userInput);    
+})
+
+$("#09 .description").val(localStorage.getItem("09"));
+$("#10 .description").val(localStorage.getItem("10"));
+$("#11 .description").val(localStorage.getItem("11"));
+$("#12 .description").val(localStorage.getItem("12"));
+$("#13 .description").val(localStorage.getItem("13"));
+$("#14 .description").val(localStorage.getItem("14"));
+$("#15 .description").val(localStorage.getItem("15"));
+$("#16 .description").val(localStorage.getItem("16"));
+$("#17 .description").val(localStorage.getItem("17"));
